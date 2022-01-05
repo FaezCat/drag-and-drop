86a4294d44bf39986153572333bca3144b7f471a
Faez Catrina <faezc@hotmail.com>
2022-01-05 17:13:04 -0500

    Initial project class creation to insert form into the DOM

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById("project-input")! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+  
+    // note that .content is available on template elms specifically
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    // afterbegin inserts the new element just after the opening tag and before the first child element (none in this case)
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+// this initial form is only rendered once we instantiate ProjectInput as done below
+const projInput = new ProjectInput();
