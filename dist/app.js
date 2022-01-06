5ff76bcad0befcebd3d02b22b18aa833b28616ab
Faez Catrina <faezc@hotmail.com>
2022-01-06 17:00:43 -0500

    Complete ProjectList class declaration including renderContent and attach methods to both attach this list to the DOM and to render it

--- dist/app.js
+++ dist/app.js
@@ -45,6 +45,16 @@ class ProjectList {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.element.id = `${this.type}-projects`;
+        this.attach();
+        this.renderContent();
+    }
+    renderContent() {
+        const listId = `${this.type}-projects-list`;
+        this.element.querySelector("ul").id = listId;
+        this.element.querySelector("h2").textContent = this.type.toUpperCase() + " PROJECTS";
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
     }
 }
 class ProjectInput {
--- src/app.ts
+++ src/app.ts
@@ -61,6 +61,19 @@ class ProjectList {
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
+    this.attach();
+    this.renderContent();
+  }
+
+  private renderContent() {
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector("ul")!.id = listId;
+    this.element.querySelector("h2")!.textContent = this.type.toUpperCase() + " PROJECTS";
+  }
+
+  private attach() {
+    // beforeend => before closing tag
+    this.hostElement.insertAdjacentElement("beforeend", this.element);
   }
 }
 
