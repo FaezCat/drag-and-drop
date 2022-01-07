603bde8158dab964123639059e029a23e7da938c
Faez Catrina <faezc@hotmail.com>
2022-01-07 16:00:45 -0500

    Implement ProjectState logic within ProjectList - now renders each added unordered list item in both sets of lists

--- src/app.ts
+++ src/app.ts
@@ -91,17 +91,33 @@ class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
+  assignedProjects: any[];
 
   constructor(private type: "active" | "completed") {
     this.templateElement = document.getElementById("project-list")! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
-  
+    this.assignedProjects = [];
     // note that .content is available on template elms specifically
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
+    
+      projectState.addListener((projects: any[]) => {
+        this.assignedProjects = projects;
+        this.renderProjects();
+      });
+    
     this.attach();
     this.renderContent();
+  }
+
+  private renderProjects() {
+    const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+    for (const projItem of this.assignedProjects) {
+      const listItem = document.createElement("li");
+      listItem.textContent = projItem.title;
+      listEl.appendChild(listItem);
+    }
   }
 
   private renderContent() {
